20f65c76c673da8332d1fc1b32e3995ccaaee5f4
梁雅喬 <87177078+YA-CIAO-LIANG@users.noreply.github.com>
2021-09-06 13:11:55 +0800

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	// char s1[100] = "";    //長度限制
+	//printf("輸入字串，印出字串中的浮點數");
+	//scanf_s("%s", s1, 100);
+	char s1[] = "bubu3.14jojo6.66";
+	const char* string = s1;
+	char* stringPtr;
+	char a[] = " ";
+	int i;
+	for (i = 0; i < strlen(s1); i = i + 1)
+	{
+		strncpy(a, s1, 1);
+		printf("%s\n", a);
+	}
+
+
+	double d = strtod(string, &stringPtr);
+
+	printf("double value %.5f and the string \"%s\"\n", d, stringPtr);
+
+ }
---
+++ 第二題.cpp
@@ -0,0 +1,71 @@
+﻿// 第二題.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include<stdio.h>
+#include<ctype.h>
+
+ int upper(char *str);
+ int lower(char*str);
+ int  num(char* str);
+
+    
+ int main(void)
+ {
+	 int(*f[3])(char*) = { upper, lower,  num };         
+
+	 char str[] = "abcDEFG000";   
+
+	 for (int c = 0; c <= 2; c++)
+	 {
+		 (*f[c])(str);
+	 }
+ }
+ 
+
+ int upper(char* str)
+ {
+	  
+	 int  size = strlen(str);   //函式會回傳字串的長度，其中的＂字串＂是必要項目，即為要計算長度的 String。
+	 int count = 0;
+	 for (int c = 0; c <= size; c++)
+	 {
+		 if (isupper(str[c]))
+		 {
+			 count++;
+		 }
+	 }
+	 printf("大寫有:%d個\n", count);
+	 return 0;
+ }
+ int lower(char* str)
+
+ {
+	 int  size = strlen(str);
+	 int count = 0;
+	 for (int c = 0; c <= size; c++)
+	 {
+		 if (islower(str[c]))
+		 {
+			 count++;
+		 }
+	 }
+	 printf("小寫有:%d個\n", count);
+	 return 0;
+ }
+ int  num(char* str)
+ {
+	 int  size = strlen(str);
+	 int count = 0;
+	 for (int c = 0; c <= size; c++)
+	 {
+		 if (isdigit(str[c]))
+		 {
+			 count++;
+		 }
+	 }
+	 printf("數字:%d個\n", count);
+	 return 0;
+ }
+
+ ///////////////////////
