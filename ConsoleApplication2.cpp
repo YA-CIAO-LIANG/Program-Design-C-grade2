e948065ce046c874b35b42f4f9e47c4479c74e76
梁雅喬 <87177078+YA-CIAO-LIANG@users.noreply.github.com>
2021-09-06 13:28:07 +0800

    Add files via upload

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,42 +1,108 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
 
-void displayBits(unsigned int value);
-// prototype
+// self-referential structure
+struct listNode
+{
+    int data;
+    struct listNode* nextPtr; // pointer to next node
+};
+
+typedef  struct     mystu
+{
+    int StuNum;
+    int math;
+    int computer;
+}student;
+
+typedef struct listNode ListNode; //; synonym for struct listNode
+typedef ListNode* ListNodePtr; // synonym for ListNode*
+
+// prototypes
+void insert(ListNodePtr* sPtr, int value);
+
+
+void printList(ListNodePtr currentPtr);
+
 int main(void)
 {
-	unsigned int x;
-	unsigned int y;
-	printf("%s", "Enter a nonnegative int: ");
-	scanf_s("%u", &x);
-	displayBits(x);
 
-	printf("%s", "Enter a nonnegative int: ");
-	scanf_s("%u", &y);
-	displayBits(y);
+    ListNodePtr startPtr = NULL; // initially there are no nodes
+
+    student  mystu = { 0,0,0 };
+
+    // printf("%s", "? ");
+    // unsigned int choice; // user's choice
+    // scanf("%u", &choice);
+
+    for (int i = 0; i < 5; i = i + 1)
+    {
+        printf("第%d個:\n", i + 1);
+        printf("%s", "輸入學生學號");
+        scanf_s("\n%d", &mystu.StuNum);
+        insert(&startPtr, mystu.StuNum);
+        printf("%s", "輸入數學成績");
+        scanf_s("\n%d", &mystu.math);
+        insert(&startPtr, mystu.math);
+        printf("%s", "輸入計概成績");
+        scanf_s("\n%d", &mystu.computer);
+        insert(&startPtr, mystu.computer);
 
 
-	printf(" AND \n");
-	displayBits(x & y);
-	printf(" OR \n");
-	displayBits(x | y);
-	printf(" XOR \n");
-	displayBits(x ^ y);
-
-
-
+    }
+    printList(startPtr);
+    puts("End of run.");
 }
 
-void displayBits(unsigned int value)
+
+
+void insert(ListNodePtr* sPtr, int value)
 {
-	unsigned int displayMask = 1 << 31; //遮罩為1左移31個位元 其他的都遮起來
-	for (unsigned int c = 1; c <= 32; ++c)   //宣告c
-	{
-		putchar(value & displayMask ? '1' : '0');  // value =199 =1100011是二進位的值,印出來要用字元方式   displaymask=00000001
-		value <<= 1;                          
-		if (c % 8 == 0)
-		{
-			putchar(' ');
-		}
-	}
-	putchar('\n');
+    ListNodePtr newPtr = (ListNodePtr)malloc(sizeof(ListNode));
+    if (newPtr != NULL) { // is space available
+        newPtr->data = value; // place value in node
+        newPtr->nextPtr = NULL; // node does not link to another node
+
+        ListNodePtr previousPtr = NULL;
+        ListNodePtr currentPtr = *sPtr;
+
+        // loop to find the correct location in the list
+        while (currentPtr != NULL && value > currentPtr->data) {
+            previousPtr = currentPtr; // walk to ...
+            currentPtr = currentPtr->nextPtr; // ... next node
+        }
+
+        // insert new node at beginning of list
+        if (previousPtr == NULL) {
+            newPtr->nextPtr = *sPtr;
+            *sPtr = newPtr;
+        }
+        else { // insert new node between previousPtr and currentPtr
+            previousPtr->nextPtr = newPtr;
+            newPtr->nextPtr = currentPtr;
+        }
+    }
+    else {
+        printf("%d not inserted. No memory available.\n", value);
+    }
+}
+
+
+
+
+// print the list
+void printList(ListNodePtr currentPtr)
+{
+    // if list is empty
+
+    puts("The list is:");
+
+    // while not the end of the list
+    while (currentPtr != NULL) {
+        printf("%d --> ", currentPtr->data);
+        currentPtr = currentPtr->nextPtr;
+    }
+
+    puts("NULL\n");
+
 }
